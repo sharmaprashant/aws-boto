681169ce3c2db023f154e4d8151df652bd332e75
prashants <prashant.aws@gmail.com>
2017-09-15 13:57:35 +1000

    Added more tag like EN V

--- Running-Machine-AMI.py
+++ Running-Machine-AMI.py
@@ -10,13 +10,19 @@ response = ec2ServerImage.describe_instances(InstanceIds=[instance_id])
 
 
 def Create_Image(ami_name):
-    AMI_ID = ec2ServerImage.create_image(InstanceId=instance_id, Name=ami_name, Description='Created by Automated Boto Script', NoReboot=True, DryRun=False)
-    print "\n***********************************************************************\n"
-    print AMI_ID
-    ec2ServerImage.create_tags(Resources=[AMI_ID['ImageId']], Tags=[{'Key': 'Name', 'Value': ami_name}, {'Key': 'Created By', 'Value': 'Bamboo Server using BotoScript'},])
+    try:
+        AMI_ID = ec2ServerImage.create_image(InstanceId=instance_id, Name=ami_name, Description='Created by Automated Boto Script', NoReboot=True, DryRun=False)
+        print "\n***********************************************************************\n"
+        print AMI_ID
+        ec2ServerImage.create_tags(Resources=[AMI_ID['ImageId']], Tags=[{'Key': 'Name', 'Value': ami_name}, {'Key': 'Created By', 'Value': 'Bamboo Server using BotoScript'}, {'Key': 'ENV', 'Value': 'Prod'},])
 
-    print "\n***********************************************************************\n"
-    print ('AMI-ID is %s against InstanceId is: %s' % (AMI_ID['ImageId'], instance_id))
+        print "\n***********************************************************************\n"
+        print ('AMI-ID is %s against InstanceId is: %s' % (AMI_ID['ImageId'], instance_id))
+        pass
+    except Exception as e:
+        print "Run the run script like this: python Running"
+        raise
+
 
 
 try:
